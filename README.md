5f1c583f4b214a141a50fdfdf26de4d3f06f1c6e
toshi <k08045kk@gmail.com>
2023-08-28 15:38:43 +0900

    v4 カレントタブから親タブへ起点を変更（fix #1）
    Firefox の target="_blank"の修正
    Firefox の ピン留タブの修正

--- README.md
+++ README.md
@@ -2,9 +2,8 @@ Open Tab Next Right to Current
 ==============================
 
 
-
 ## Features
-Open a new tab created from the current tab to the right of the current tab.
+Open new tab created from current tab, next right to current tab.
 
 
 
@@ -12,21 +11,16 @@ Open a new tab created from the current tab to the right of the current tab.
 This extension affects the behavior of opening new tabs.
 
 The following behaviors are affected:
++ New tab created from current tab open next right to current tab.
+	+ Second and subsequent new tab also open next right to current tab.
+	+ New tab created from pinned tab open next right to last pinned tab.
 
-+ New tab for the current tab will open to the right of the current tab.
-	+ The second and subsequent new tabs will also open to the right of the current tab.
-	+ New tab for a pinned tab will open to the right of the last pinned tab.
-
-The following behaviors are unaffected:
-
-+ New tab for a window will open to the right of the window.
-+ New tab for Shortcut will open to the right of the window.
-+ New tab for Bookmark will open to the right of the window.
-+ New tab for Reading List will open to the right of the window.
-+ New tab for History will open to the right of the window.
-+ New tab for Tab Context Menu will open to the right of the current tab.
-+ Open Closed Tab open the tab in its original location.
-+ Restore window open in its original state.
+The following behaviors are controlled so that they are not affected:
++ New tab created from Window open to right edge the window.
++ New tab created from Shortcut open to right edge the window.
++ New tab created from Bookmark open to right edge the window.
++ New tab created from Tab Contextmenu open next right to current tab.
++ Restore closed tab/window to original state.
 
 *This does not apply if it interferes with other extensions or settings.
 
--- src/background.js
+++ src/background.js
@@ -14,48 +14,49 @@ const moveTab = async (newTab) => {
     return;
   }
   if (newTab.active === true) {
-    // アクティブタブ（ウィンドウ・タブコンテキストメニュー・ショートカット・履歴）
+    // アクティブタブ（ウィンドウ・ショートカット・履歴・タブコンテキストメニュー）
+    // 備考：Firefox の target="_blank" を除く（この時点では、まだ非アクティブ扱い）
     return;
   }
   
   
-  const windowId = newTab.windowId;
-  const info = {populate:true};
-  const win = await chrome.windows.get(windowId, info);
-  if (!win) {
-    return;
-  }
-  //console.log('win', win);
-  
-  
-  let currentTab = null;
-  for (const tab of win.tabs) {
-    if (tab.active) {
-      currentTab = tab;
-      break;
+  const openerTab = await chrome.tabs.get(newTab.openerTabId);
+  //console.log('openerTab', openerTab);
+  if (openerTab && openerTab.windowId === newTab.windowId) {
+    // 親タブ（過去のカレントタブ）の右隣へ移動する
+    let index = openerTab.index + 1;
+    if (openerTab.pinned) {
+      // 親タブがピン留めタブの場合、最後のピン留めタブの右隣に移動する
+      // 備考：ピン留めタブの途中へ移動する場合、動作が異なる。
+      //       Firefox は、無効扱いとなり移動しない。
+      //       Chrome は、自動で最後のピン留めタブの右隣に移動する。
+      const win = await chrome.windows.get(openerTab.windowId, {populate:true});
+      if (win) {
+        for (const tab of win.tabs) {
+          if (!tab.pinned) {
+            index = tab.index;
+            break;
+          }
+        }
+      }
     }
-  }
-  if (!currentTab) {
-    return;
-  }
-  //console.log('currentTab', currentTab);
-  
-  
-  const newIndex = currentTab.index + 1;
-  if (newTab.index !== newIndex) {
-    //console.log('move', newTab.index, '->', newIndex);
-    const movedTab = await chrome.tabs.move(newTab.id, {index:newIndex});
-    //console.log('movedTab', movedTab);
+    if (index !== newTab.index) {
+      //console.log('move', newTab.index, '->', index);
+      const movedTab = await chrome.tabs.move(newTab.id, {index:index});
+      //console.log('movedTab', movedTab);
+    }
+    // 備考：openerTab がアクティブ（カレントタブ）であるかを考慮しない
+    //       Firefox で非アクティブの可能性がある（target="_blank"）
   }
   
   // 備考：新規タブを生成しないページ移動は、影響を受けません。
   //       新規タブが以前に開いたタブの場合、影響を受けません。
   //       新規タブが親のいないタブの場合、影響を受けません。
-  //       新規タブがアクティブタブの場合、影響を受けません。
-  //       上記以外の場合、カレントタブの右隣に開きます。
+  //       新規タブがアクティブタブの場合、影響を受けません。（Firefox target="_blank"を除く）
+  //       上記以外の場合、カレントタブ（親タブ）の右隣に開きます。
   // 備考：次の動作は、影響を受けます。
-  //         カレントタブからの新規タブは、カレントタブの右隣に開きます。
-  //           ２つめ以降の新規タブも、カレントタブの右隣に開きます。
+  //         カレントタブからの新規タブは、カレントタブ（親タブ）の右隣に開きます。
+  //           ２つめ以降の新規タブも、カレントタブ（親タブ）の右隣に開きます。
   //           ピン留めされている場合、最後のピン留めタブの右隣に開きます。
   //       次の動作は、影響を受けません。
   //         ウィンドウからの新規タブは、ウィンドウの右端に開きます。
@@ -67,6 +68,9 @@ const moveTab = async (newTab) => {
   //         閉じたタブを開くは、元の場所に開きます。
   //         ウィンドウの復元は、元の状態で開きます。
   //       他の拡張機能または設定と干渉する場合は、この限りではありません。
+  // 備考：target="_blank" の挙動が Chrome と Firefox で異なる。
+  //       Chrome は、カレントタブの右隣に新規タブを開く。
+  //       Firefox は、直前に開いたタブの右隣に新規タブを開く。
 };
 
 
